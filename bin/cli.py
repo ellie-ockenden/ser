7f3a3252edc1b8ac6c1ad47bc3b9b1c3d1a7125c
eockenden <eockenden@C02FWYSVQ6L4.local>
2021-11-23 15:39:19 +0000

    with savig the model

--- bin/cli.py
+++ bin/cli.py
@@ -5,114 +5,70 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+import datetime
 
+from ser.model import make_model
+from ser.transforms import transform
+from ser.data import train_data_load, val_data_load
+from ser.train import train_model
 import typer
+from dataclasses import dataclass
+import dataclasses
+import json
 
 main = typer.Typer()
 
 PROJECT_ROOT = Path(__file__).parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 
+@dataclass
+class HyperParams:
+    epochs: int
+    batch_size: int
+    learning_rate: float
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        return super().default(o)
 
 @main.command()
 def train(
     name: str = typer.Option(
         ..., "-n", "--name", help="Name of experiment to save under."
     ),
+    epochs: int = typer.Option(2, "-eps", "--epochs", help="Number of epochs"
+    ),
+    batch_size: int = typer.Option(1000, "-bs", "--batch_size", help="Batch size"
+    ),
+    learning_rate: float = typer.Option(0.01, "-lr", "--learning_rate", help="Learning rate"
+    ),
+
 ):
     print(f"Running experiment {name}")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    epochs = 2
-    batch_size = 1000
-    learning_rate = 0.01
 
-    # save the parameters!
+    current_params = HyperParams(epochs, batch_size, learning_rate)
+    current_date = datetime.now()
+    date_string =current_date.strftime("%d-%b-%Y_(%H:%M:%S)")
+    RUN_DIR = PROJECT_ROOT / "run" / name / date_string
+    with open(RUN_DIR / "config.json", "w") as fjson:
+        json.dump(current_params, fjson, cls=EnhancedJSONEncoder)
 
     # load model
-    model = Net().to(device)
 
-    # setup params
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    model, optimizer = make_model(learning_rate, device)
 
-    # torch transforms
-    ts = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
-    )
+    ts = transform()
 
-    # dataloaders
-    training_dataloader = DataLoader(
-        datasets.MNIST(root="../data", download=True, train=True, transform=ts),
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=1,
-    )
-
-    validation_dataloader = DataLoader(
-        datasets.MNIST(root=DATA_DIR, download=True, train=False, transform=ts),
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=1,
-    )
-
-    # train
-    for epoch in range(epochs):
-        for i, (images, labels) in enumerate(training_dataloader):
-            images, labels = images.to(device), labels.to(device)
-            model.train()
-            optimizer.zero_grad()
-            output = model(images)
-            loss = F.nll_loss(output, labels)
-            loss.backward()
-            optimizer.step()
-            print(
-                f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
-                f"| Loss: {loss.item():.4f}"
-            )
-            # validate
-            val_loss = 0
-            correct = 0
-            with torch.no_grad():
-                for images, labels in validation_dataloader:
-                    images, labels = images.to(device), labels.to(device)
-                    model.eval()
-                    output = model(images)
-                    val_loss += F.nll_loss(output, labels, reduction="sum").item()
-                    pred = output.argmax(dim=1, keepdim=True)
-                    correct += pred.eq(labels.view_as(pred)).sum().item()
-                val_loss /= len(validation_dataloader.dataset)
-                val_acc = correct / len(validation_dataloader.dataset)
-
-                print(
-                    f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
-                )
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
-
-
+    training_dataloader = train_data_load(batch_size, ts, DATA_DIR)
+    validation_dataloader = val_data_load(batch_size, ts, DATA_DIR)
+ 
+    train_model(epochs, training_dataloader, validation_dataloader, model, optimizer, device)
+               
+    torch.save(model, RUN_DIR / "model.pt")
+    
 @main.command()
 def infer():
     print("This is where the inference code will go")
